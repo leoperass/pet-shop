74e0bb7b072f6e10058a482663f81620e68d41ed
Leonardo Perassoli <leonardoperass@gmail.com>
2026-09-15 19:27:48 -0300

    chore: testing lefthook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 type Props = {
   title: string;
@@ -16,7 +16,7 @@ const Component = ({ title }: Props) => {
 export default function Home() {
   return (
     <div>
-      <h2>TESTE</h2>
+      <h2>TESTE 2</h2>
 
       <Component title={1} />
     </div>
