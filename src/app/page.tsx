6ff01d6120df7b8453862e6a2bad2f549469dc4e
Leonardo Perassoli <leonardoperass@gmail.com>
2026-09-15 19:10:37 -0300

    chore: testing lefthook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const Component = ({ title }: Props) => {
 export default function Home() {
   return (
     <div>
-      <h2>Home</h2>
+      <h2>TESTE</h2>
 
       <Component title={1} />
     </div>
